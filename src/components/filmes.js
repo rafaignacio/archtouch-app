4315b63df831a4fd81f636347b7a09b051bde2ee
usuario <usuario@localhost.localdomain>
2019-03-13 15:22:20 -0300

    ajuste

--- src/components/filmes.js
+++ src/components/filmes.js
@@ -12,17 +12,34 @@ import moment from "moment";
 export default class FilmesComponent extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       isLoading: false,
       data: [],
+      genres: [],
       page: 1,
       totalPages: 1
     };
   }
 
+  obterGeneros = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=1f54bd990f1cdfb230adb312546d765d`
+    );
+    const genres = await response.json();
+
+    return genres;
+  };
+
   obterFilmes = async () => {
     const { page, totalPages } = this.state;
     if (this.state.isLoading || totalPages > page) return;
+
+    if(this.state.genres.length <= 0) {
+      const genres = await this.obterGeneros();
+
+      this.setState({ genres: genres.genres });
+    }
 
     this.setState({ isLoading: true });
 
@@ -40,6 +57,21 @@ export default class FilmesComponent extends Component {
 
   onPress = () => {};
 
+  findGenreById = (id) => {
+    const item = this.state.genres.find( g => g.id == id);
+
+    return item;
+  }
+
+  getGenresName = (genres) => {
+    let output = '';
+    genres.forEach( g => {
+      output += this.findGenreById(g).name + ', ';
+    })
+
+    return output.substring(0, output.length - 2);
+  }
+
   renderItem = ({ item }) => {
     const path = `https://image.tmdb.org/t/p/w500${item.poster_path ||
       item.backdrop_path}`;
@@ -50,7 +82,8 @@ export default class FilmesComponent extends Component {
           style={{
             flexDirection: "row",
             paddingBottom: 20,
-            paddingRight: 5
+            paddingRight: 5,
+            marginTop: 4,
           }}
         >
           <View style={{ flex: 0.3 }}>
@@ -76,12 +109,19 @@ export default class FilmesComponent extends Component {
             >
               {item.title}
             </Text>
-            <Text style={{ fontSize: 14, fontWeight: "500" }}>
-              {moment(Date.parse(item.release_date)).format("DD/MM/YYYY")}
+            <Text style={{ fontSize: 14, fontWeight: "600" }}>
+              <Text style={{fontWeight: '200'}}>Release Date: </Text> {moment(Date.parse(item.release_date)).format("DD/MM/YYYY")}
             </Text>
+            {
+              <View style={{ flexDirection: 'row'}}>
+                <Text style={{fontWeight: '100'}}>Genres: </Text>
+                <Text>{this.getGenresName(item.genre_ids)}</Text>
+              </View>
+            }
             <Text
               style={{
-                justifyContent: "center"
+                justifyContent: "center",
+                marginTop: 6,
               }}
             >
               {item.overview}
