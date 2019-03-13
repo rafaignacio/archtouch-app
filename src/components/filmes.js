4b5b5fdfe7f016526c763848bbeb25c2b996798e
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2019-03-12 22:19:33 -0300

    ajustes

--- src/components/filmes.js
+++ src/components/filmes.js
@@ -1,51 +1,120 @@
-import React, { Component } from 'react'
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
-
-async function getMoviesFromApi() {
-    try {
-        let response = await fetch(
-            'https://facebook.github.io/react-native/movies.json',
-        );
-        let responseJson = await response.json();
-
-        return responseJson.movies;
-    } catch (error) {
-        console.error(error);
-    }
-}
+import React, { Component } from "react";
+import {
+  FlatList,
+  ActivityIndicator,
+  Text,
+  View,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import moment from "moment";
 
 export default class FilmesComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={ isLoading: true}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      data: [],
+      page: 1,
+      totalPages: 1
+    };
+  }
 
-    async componentDidMount(){
-        return getMoviesFromApi().then((movies) => {
-            this.setState({
-                isLoading: false,
-                dataSource: movies,
-            });
-        });
-    }
+  obterFilmes = async () => {
+    const { page, totalPages } = this.state;
+    if (this.state.isLoading || totalPages > page) return;
 
-    render(){
+    this.setState({ isLoading: true });
 
-        if(this.state.isLoading){
-          return(
-            <View style={{flex: 1, padding: 20}}>
-              <ActivityIndicator/>
-            </View>
-          )
-        }
-    
-        return(
-          <View style={{flex: 1, paddingTop:20}}>
-            <FlatList
-              data={this.state.dataSource}
-              renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-              keyExtractor={({id}, index) => id} />
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=1f54bd990f1cdfb230adb312546d765d&page=${page}`
+    );
+    const movies = await response.json();
+
+    this.setState({
+      data: [...this.state.data, ...movies.results],
+      page: page + 1,
+      isLoading: false
+    });
+  };
+
+  onPress = () => {};
+
+  renderItem = ({ item }) => {
+    const path = `https://image.tmdb.org/t/p/w500${item.poster_path ||
+      item.backdrop_path}`;
+
+    return (
+      <TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            paddingBottom: 20,
+            paddingRight: 5
+          }}
+        >
+          <View style={{ flex: 0.3 }}>
+            <Image
+              style={{
+                width: 110,
+                height: 110,
+                resizeMode: "contain"
+              }}
+              source={{
+                uri: path
+              }}
+            />
           </View>
-        );
-      }
+          <View style={{ flexDirection: "column", flex: 0.9 }}>
+            <Text
+              style={{
+                fontSize: 15,
+                fontWeight: "600",
+                flexWrap: "wrap"
+              }}
+              ellipsizeMode={"tail"}
+            >
+              {item.title}
+            </Text>
+            <Text style={{ fontSize: 14, fontWeight: "500" }}>
+              {moment(Date.parse(item.release_date)).format("DD/MM/YYYY")}
+            </Text>
+            <Text
+              style={{
+                justifyContent: "center"
+              }}
+            >
+              {item.overview}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  renderFooter = () => {
+    if (!this.state.isLoading) return null;
+    return (
+      <View>
+        <ActivityIndicator />
+      </View>
+    );
+  };
+
+  async componentDidMount() {
+    this.obterFilmes();
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={({ id }, index) => id.toString()}
+        onEndReached={this.obterFilmes}
+        onEndReachedThreshold={0.15}
+        ListFooterComponent={this.renderFooter}
+      />
+    );
+  }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { View, Text, } from 'react-native';
-import FilmesComponent from './filmes';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import FilmesComponent from "./filmes";
 
 class HomeScreen extends Component {
-    static navigationOptions = {
-        title: 'Filmes',
-    };
+  static navigationOptions = {
+    title: "Filmes"
+  };
 
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <FilmesComponent />
-        </View>
-      );
-    }
-};
+  render() {
+    return (
+      <View>
+        <FilmesComponent />
+      </View>
+    );
+  }
+}
 
 export default HomeScreen;
